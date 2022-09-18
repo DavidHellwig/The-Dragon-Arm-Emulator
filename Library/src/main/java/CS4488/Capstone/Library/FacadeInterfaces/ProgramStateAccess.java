f4e8c4d431be83cf33578054cbda9ab737d304a0
Traae <bloxtraa@isu.edu>
2022-09-18 10:19:03 -0600

    worked on Hex4d, not done 3

--- Library/src/main/java/CS4488/Capstone/Library/FacadeInterfaces/ProgramStateAccess.java
+++ Library/src/main/java/CS4488/Capstone/Library/FacadeInterfaces/ProgramStateAccess.java
@@ -1,15 +1,25 @@
 package CS4488.Capstone.Library.FacadeInterfaces;
-
 import CS4488.Capstone.Library.Tools.Hex4digit;
+import CS4488.Capstone.Library.Tools.ProgramState;
 
 import java.util.ArrayList;
 
+/**
+ * This interface dictates the method of access to the Program state by the UI
+ */
 public interface ProgramStateAccess {
-    public Hex4digit[] getRegistersValues();
-    public Hex4digit getPriorPCValue();
-    public void enterInput(Hex4digit input);
-    public Hex4digit getOutput();
-    public ArrayList<Hex4digit> getBaseMemoryValues();
-    public ArrayList<Hex4digit> getCurrentMemoryValues();
-    public ArrayList<Hex4digit[]> getMemoryStateGrid();
+    /**
+     * This method provides a copy of the program state,
+     * To ensure the actual state isn't accessible.
+     *
+     * @return a Copy Array Program State
+     */
+    public ProgramState getProgramStateCopy();
+
+    /**
+     * The Input must be passed into the backend ahead of it being 'read' byt the emulation.
+     *
+     * @param in the input in the Input Box.
+     */
+    public void sendInput(Hex4digit in);
 }
