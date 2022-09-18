b0c1308d4c54acda8f20826f0c03662b035b06ef
igbodani <igbodani@isu.edu>
2022-09-17 22:52:59 -0600

    started parsing quick code to hex

---
+++ Translator/src/main/java/CS4488/Capstone/Translator/InstructionParser.java
@@ -0,0 +1,60 @@
+package CS4488.Capstone.Translator;
+
+import java.util.*;
+
+public class InstructionParser {
+    private final Map<String, String> parser
+            = new HashMap<>();
+
+    public InstructionParser(){
+        loadInstructions();
+        loadRegisters();
+        loadMemory();
+    }
+
+    public Map<String, String> getParser() {
+        return parser;
+    }
+
+    private void loadInstructions(){
+        parser.put("hlt", "0");
+        parser.put("ld", "1");
+        parser.put("st", "2");
+        parser.put("add", "3");
+        parser.put("sub", "4");
+        parser.put("br", "9");
+
+    }
+
+
+    private void loadRegisters(){
+        for(int i = 0; i < 16; i++){
+            parser.put("r"+Integer.toHexString(i), Integer.toHexString(i));
+        }
+    }
+
+
+    private void loadMemory(){
+        for(int i = 0; i < 16; i++){
+            for(int j = 0; j < 16; j++){
+                parser.put("m"+Integer.toHexString(i)+ Integer.toHexString(j),
+                        Integer.toHexString(i)+ Integer.toHexString(j));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        InstructionParser instructionParser= new InstructionParser();
+
+        for (Map.Entry<String, String> me :
+                instructionParser.parser.entrySet()) {
+
+            // Printing keys
+            System.out.print(me.getKey() + ":");
+            System.out.println(me.getValue());
+        }
+
+    }
+
+
+}
--- Translator/src/main/java/CS4488/Capstone/Translator/Translator.java
+++ Translator/src/main/java/CS4488/Capstone/Translator/Translator.java
@@ -5,21 +5,36 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Translator implements TranslatorInterface{
 
+    private String file;
+    private boolean loaded;
 
+    public Translator(String file) throws Exception {
+        this.file = "";
+        this.loaded = loadFile(file);
+    }
+
+    public void setLoaded(boolean loaded) {
+        this.loaded = loaded;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
 
     @Override
     public boolean loadFile(String path) throws Exception {
-        String file = readFile(path);
-
-        return !file.equals("");
+         this.file = readFile(path);
+        return !this.file.equals("");
     }
 
     @Override
     public void clearFile() {
-
+        setFile("");
+        setLoaded(false);
     }
 
     @Override
@@ -45,10 +60,39 @@ public class Translator implements TranslatorInterface{
 
     public String [] parseFile(String armFile){
         String noComments = armFile.replaceAll("@[a-zA-z0-9 ]+@", "").replaceAll("\\s", "");
+        //noComments.strip()
 
         //remove all white space .replaceAll("\\s", "")
-
         return noComments.split(";");
 
+    }
+
+    public ArrayList<String> convertToHex(String [] parsedFile){
+        InstructionParser instructionParser = new InstructionParser();
+
+        ArrayList<String> hexFile = new ArrayList<>();
+        for(int i = 0; i< parsedFile.length; i++){
+            //InstructionParser.parser
+            for (Map.Entry<String, String> me :
+                    instructionParser.getParser().entrySet()) {
+                String key = me.getKey();
+                String value = me.getValue();
+                parsedFile[i] = parsedFile[i].replaceAll(key, value);
+            }
+
+            hexFile.add(parsedFile[i].trim());
+        }
+
+        return hexFile;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Translator translator = new Translator("Translator/src/main/java/CS4488/Capstone/Translator/armcode.txt");
+        ArrayList<String> list = translator.convertToHex(translator.parseFile(translator.file));
+
+        for(String file : list){
+            System.out.println(file);
+        }
+//        System.out.println(translator.file);
     }
 }
