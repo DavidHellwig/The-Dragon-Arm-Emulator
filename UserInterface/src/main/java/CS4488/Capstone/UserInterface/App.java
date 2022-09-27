98db971db078ed791c27ee71dd64dbb1e29ba9cb
David Hellwig <helldavi@isu.edu>
2022-09-26 22:06:51 -0600

    Slight GUI changes, changing size of screen

--- UserInterface/src/main/java/CS4488/Capstone/UserInterface/App.java
+++ UserInterface/src/main/java/CS4488/Capstone/UserInterface/App.java
@@ -23,7 +23,7 @@ public class App extends Application {
         //I don't know why but my IDE automatically changed this line. If anything breaks this might be worth looking at
         Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/EmulatorGUI.fxml")));
 
-        Scene scene = new Scene(parent);
+        Scene scene = new Scene(parent,1280,720);
 
         primaryStage.setScene(scene);
 
