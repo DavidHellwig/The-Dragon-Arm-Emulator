372eb4a5ce63f6aad9907c99da6aef6c3a5ac87b
Traae <bloxtraa@isu.edu>
2022-10-31 10:19:14 -0600

    quick troubleshoot

---
+++ System/src/main/java/CS4488/Capstone/System/terminal.java
@@ -0,0 +1,50 @@
+package CS4488.Capstone.System;
+
+import CS4488.Capstone.Library.Tools.Hex4digit;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class terminal {
+
+    public static int main(String[] args) {
+
+        boolean keepGoing = true;
+        Scanner in = new Scanner(System.in);
+        Orchestrator o = Orchestrator.getInstance();
+        while (keepGoing) {
+            System.out.println("Options:\n" +
+                    "0. exit\n" +
+                    "1. Translate and load Directory\n" +
+                    "2. Next\n" +
+                    "3. Set Input\n" +
+                    "4. Clear Program\n");
+            int a = in.nextInt();
+            switch (a) {
+                case 0:
+                    keepGoing = false;
+                    break;
+                case 1:
+                    String s = in.nextLine();
+                    o.translateAndLoad(s);
+                    o.getProgramState().printProgramState();
+                    break;
+                case 2:
+                    o.next();
+                    o.getProgramState().printProgramState();
+                    break;
+                case 3:
+                    String hex = in.nextLine();
+                    o.getProgramState().input = new Hex4digit(hex);
+                    break;
+                case 4:
+                    o.clearProgram();
+                    break;
+                default:
+                    break;
+            }
+        }
+        return 0;
+    }
+}
+
--- System/src/test/java/CS4488/Capstone/System/OrchestratorTest.java
+++ System/src/test/java/CS4488/Capstone/System/OrchestratorTest.java
@@ -10,43 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class OrchestratorTest {
     Orchestrator orchestrator = Orchestrator.getInstance();
 
-    @Test void terminalTester() {
-        boolean keepGoing = true;
-        Scanner in = new Scanner(System.in);
-        Orchestrator o = Orchestrator.getInstance();
-        while (keepGoing) {
-            System.out.println("Options:\n" +
-                    "0. exit\n" +
-                    "1. Translate and load Directory\n" +
-                    "2. Next\n" +
-                    "3. Set Input\n" +
-                    "4. Clear Program\n");
-            int a = in.nextInt();
-            switch (a) {
-                case 0:
-                    keepGoing = false;
-                    break;
-                case 1:
-                    String s = in.nextLine();
-                    o.translateAndLoad(s);
-                    o.getProgramState().printProgramState();
-                    break;
-                case 2:
-                    o.next();
-                    o.getProgramState().printProgramState();
-                    break;
-                case 3:
-                    String hex = in.nextLine();
-                    o.getProgramState().input = new Hex4digit(hex);
-                    break;
-                case 4:
-                    o.clearProgram();
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
+
 
     @Test
     void next() {
