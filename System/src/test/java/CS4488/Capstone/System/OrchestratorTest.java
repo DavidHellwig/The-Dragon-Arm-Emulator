11a59a781b07524d70c23c4fae03ba36ea3cff85
Traae <bloxtraa@isu.edu>
2022-11-21 10:06:29 -0700

    Functionality looking good, updated tests

--- System/src/test/java/CS4488/Capstone/System/OrchestratorTest.java
+++ System/src/test/java/CS4488/Capstone/System/OrchestratorTest.java
@@ -10,7 +10,6 @@ class OrchestratorTest {
     private static final String resources = "./ResourceDirectories";
     String Program1 = resources + "/Example Code/Program 1, Hello Branch and Math.txt";
     String ProgramXYZ = resources + "/Example Code/Program XYZ, TestingCoverage.txt";
-    String ProgramSafexyz = resources + "/Example Code/Program xyz-Safe Labels, TestingCoverage.txt";
     String fakeFile = resources + "/Example Code/fhw4fhq2fhq4thq.txt";
     String badFile = resources + "/Example Code/Program X, Bad Program.txt";
 
@@ -28,14 +27,37 @@ class OrchestratorTest {
     String branchPositive = resources + "/Example Code/branch pos test.txt";
     String read = resources + "/Example Code/read test.txt";
     String write = resources + "/Example Code/write test.txt";
+    String skip = resources + "/Example Code/skip test.txt";
+
+    void runTestProgramWithTranslation(String file, int input, int stepLimit){
+            System.out.println("Running program from \n" + file);
+            int i = 0;
+            orchestrator.translateAndLoad(file);
+            System.out.println("\n T R A N S L A T O R  -  D O N E \n");
+            for (Hex4digit h: orchestrator.getProgramState().memoryStateHistory.get(0)){
+                System.out.println(h.getString());
+            }
+
+            orchestrator.getProgramState().input = new Hex4digit(input);
+            System.out.println("Initial State, State " + i + ": \n");
+            System.out.println(orchestrator.getProgramState().printableProgramState());
+            while ((orchestrator.next()) && (stepLimit > -1)){
+                stepLimit--;
+                i++;
+                System.out.println("\nState " + i + ": \n");
+                System.out.println(orchestrator.getProgramState().printableProgramState());
+            }
+            orchestrator.clearProgram();
+    }
 
     @Test
-    @DisplayName("Execution and Print")
+    @DisplayName("Individual tests")
     void executionAndPrint() {
 
-        // I have commented out the Programs that I've verified - Traae
+        System.out.println("\nXYZ Test\n");
+        runTestProgramWithTranslation(ProgramXYZ, 4097, 20);
 
-        //runProgram(Program1, 1, 10);
+        // I have commented out the Programs that I've verified - Traae
 
         //System.out.println("\nLoad Test\n");
         //runProgram(load, 25, 10);
@@ -59,32 +81,35 @@ class OrchestratorTest {
         //System.out.println("\nLoad Indirect Test\n");
         //runProgram(loadIndirect, 25, 10);
 
-        // TODO needs fixed
+
         //System.out.println("\nStore Indirect Test\n");
         //runProgram(storeIndirect, 25, 10);
 
-        System.out.println("\nBranch Test\n");
-        runProgram(branch, 25, 10);
+        //System.out.println("\nBranch Test\n");
+        //runProgram(branch, 25, 10);
 
 
-        System.out.println("\nBranch Zero Test\n");
-        runProgram(branchZero, 25, 10);
+       //System.out.println("\nBranch Zero Test\n");
+        //runProgram(branchZero, 25, 10);
 
 
-        System.out.println("\nBranch Negative Test\n");
-        runProgram(branchNegative, 25, 10);
+        //System.out.println("\nBranch Negative Test\n");
+        //runProgram(branchNegative, 25, 10);
 
-        // TODO Isn't Translating
-        System.out.println("\nBranch Positive Test\n");
-        runProgram(branchPositive, 25, 10);
 
-        //TODO Bugged
-        System.out.println("\nRead Test\n");
-        runProgram(read, 25, 10);
+        //System.out.println("\nBranch Positive Test\n");
+        //runProgram(branchPositive, 25, 10);
 
-        // TODO BUGGED
-        System.out.println("\nWrite Test\n");
-        runProgram(write, 25, 10);
+
+        //System.out.println("\nRead Test\n");
+        //runProgram(read, 25, 10);
+
+
+        //System.out.println("\nWrite Test\n");
+        //runProgram(write, 25, 10);
+
+        //System.out.println("\nskip Test\n");
+        //runProgram(skip, 25, 10);
 
 
     }
